8fac9ab24bf0fcedcaf2e56fcb880c92ce353aaf
ColorFilter <johan@romantech.net>
2025-02-12 14:50:54 +0900

    :package: Shadcn 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <div>
       <h1>안녕하세요 </h1>
+      <Button variant="outline">Button</Button>
     </div>
   );
 }
