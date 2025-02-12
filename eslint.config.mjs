c96d24495227d325298409d77ab4fd2c035a6deb
ColorFilter <johan@romantech.net>
2025-02-12 14:52:12 +0900

    📦 필수 패키지 설치 (#1)
    * :package: ESLint + Prettier 설정
    
    * :bento: pretendard 폰트 설정
    
    * :package: Shadcn 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierPluginRecommended from 'eslint-plugin-prettier/recommended';
+import tailwindPlugin from 'eslint-plugin-tailwindcss';
+import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +13,28 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default tseslint.config(
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...tailwindPlugin.configs['flat/recommended'],
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  prettierPluginRecommended,
 
-export default eslintConfig;
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+
+  {
+    rules: {
+      'prettier/prettier': 'warn',
+      // 타입 import 할 때 인라인으로 type 키워드 추가 e.g., import { type Circle } from '...'
+      '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
+    },
+  },
+);
