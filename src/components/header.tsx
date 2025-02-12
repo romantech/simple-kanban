bd579b5b351d69ba3f846de9c3721a289ff4d9f1
ColorFilter <johan@romantech.net>
2025-02-13 01:31:38 +0900

    :lipstick: 이미지 사이즈 수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="flex h-[85px] items-center border-b border-line bg-haiti text-2xl font-extrabold">
       <div className="flex h-full min-w-72 items-center border-r border-line bg-haiti px-6">
-        <Image src="logo-light.svg" alt="logo" width={160} height={80} />
+        <Image src="logo-light.svg" alt="logo" width={153} height={26} className="size-auto" />
       </div>
       <h2 className="p-6">{title}</h2>
     </header>
