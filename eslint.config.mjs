de405feec50a3b7ed14e8485bba3b400d9b74e63
ColorFilter <johan@romantech.net>
2025-02-12 13:37:05 +0900

    :package: ESLint + Prettier 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierPluginRecommended from 'eslint-plugin-prettier/recommended';
+import tailwindPlugin from 'eslint-plugin-tailwindcss';
+import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +13,22 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default tseslint.config(
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...tailwindPlugin.configs['flat/recommended'],
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  prettierPluginRecommended,
 
-export default eslintConfig;
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+
+  { rules: { 'prettier/prettier': 'warn' } },
+);
