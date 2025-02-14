3051e5438dde659c589931d927917e3a7a2095c5
ColorFilter <johan@romantech.net>
2025-02-14 16:30:03 +0900

    :triangular_flag_on_post: 컬럼 드래그 이동 구현

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import { AddColumnDialog, Column } from '@/components';
 import { useKanbanStore } from '@/store';
 import { useShallow } from 'zustand/react/shallow';
 import { Button } from '@/components/ui/button';
 import { SquarePlus } from 'lucide-react';
+import { DndContext, type DragEndEvent, DragOverlay, type DragStartEvent } from '@dnd-kit/core';
+import { arrayMove, SortableContext } from '@dnd-kit/sortable';
+import { useState } from 'react';
+import { type ColumnId } from '@/lib';
+import { restrictToHorizontalAxis, restrictToParentElement } from '@dnd-kit/modifiers';
 
 const Board = () => {
   // useShallow: 셀렉터 반환값의 "얕은 비교" 수행
@@ -10,11 +17,40 @@ const Board = () => {
   // 단일 원시 타입 값은 기본 비교로 충분하므로 useShallow 불필요
   const board = useKanbanStore(useShallow(({ boards, currentBoardId }) => boards[currentBoardId]));
 
+  const editColumnOrder = useKanbanStore.use.editColumnOrder();
+
+  const [activeId, setActiveId] = useState<ColumnId>();
+
+  const onDragStart = (event: DragStartEvent) => {
+    setActiveId(event.active.id as ColumnId);
+  };
+
+  const onDragEnd = ({ active, over }: DragEndEvent) => {
+    if (active.id === over?.id) return;
+
+    const oldIndex = board.columnIds.findIndex((id) => id === active.id);
+    const newIndex = board.columnIds.findIndex((id) => id === over?.id);
+
+    const newColumnIds = arrayMove(board.columnIds, oldIndex, newIndex);
+    editColumnOrder(board.id, newColumnIds);
+  };
+
   return (
     <div className="scroll-custom flex w-full gap-4 overflow-x-auto p-6">
-      {board.columnIds.map((columnId) => (
-        <Column key={columnId} columnId={columnId} />
-      ))}
+      <DndContext
+        onDragEnd={onDragEnd}
+        onDragStart={onDragStart}
+        modifiers={[restrictToHorizontalAxis]}
+      >
+        <SortableContext items={board.columnIds}>
+          {board.columnIds.map((columnId) => (
+            <Column key={columnId} columnId={columnId} />
+          ))}
+        </SortableContext>
+        <DragOverlay modifiers={[restrictToParentElement]}>
+          {activeId && <Column columnId={activeId} className="pointer-events-none" />}
+        </DragOverlay>
+      </DndContext>
 
       <div className="ml-auto mt-11 flex">
         <AddColumnDialog boardId={board.id}>
