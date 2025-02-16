d9e780411f1f973126bd51b2bb63447d793f46a8
ColorFilter <johan@romantech.net>
2025-02-16 17:50:05 +0900

    :lipstick: 컬럼 헤더 애니메이션 추가

--- src/components/kanban-column/column-header.tsx
+++ src/components/kanban-column/column-header.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { type ChangeEvent, type HTMLAttributes, type KeyboardEvent, useState } from 'react';
-import { EditIcon, Save, Trash2 } from 'lucide-react';
+import { EditIcon, GripVertical, Save, Trash2 } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import { ConfirmDialog } from '@/components/ui/confirm-dialog';
 import { cn } from '@/lib';
 import { IconButton } from '@/components/ui/icon-button';
+import { AnimatePresence, motion, type MotionProps } from 'motion/react';
 
 interface ColumnHeaderProps extends HTMLAttributes<HTMLDivElement> {
   title: string;
@@ -13,6 +14,13 @@ interface ColumnHeaderProps extends HTMLAttributes<HTMLDivElement> {
   onDelete: () => void;
   onTitleChange: (title: string) => void;
 }
+
+const fadeScaleAnimation: MotionProps = {
+  initial: { scaleX: 0, opacity: 0 },
+  animate: { scaleX: 1, opacity: 1 },
+  exit: { scaleX: 0, opacity: 0 },
+  transition: { duration: 0.2 },
+};
 
 const ColumnHeader = ({
   title,
@@ -57,28 +65,46 @@ const ColumnHeader = ({
       )}
       {...divProps}
     >
-      {isEditing ? (
-        <Input
-          value={editedTitle}
-          onChange={onInputChange}
-          onBlur={saveTitle}
-          onKeyDown={onKeyDown}
-          className="h-6 px-2 font-bold focus-visible:ring-0"
-          autoFocus
-        />
-      ) : (
-        <h3 className="line-clamp-1 text-sm font-bold">{`${title} (${taskCount})`}</h3>
-      )}
+      <GripVertical className="size-5 flex-none" />
+      <AnimatePresence mode="wait">
+        {isEditing ? (
+          <motion.div
+            key="input"
+            {...fadeScaleAnimation}
+            style={{ transformOrigin: 'right' }}
+            className="grow"
+          >
+            <Input
+              value={editedTitle}
+              onChange={onInputChange}
+              onBlur={saveTitle}
+              onKeyDown={onKeyDown}
+              className="h-6 px-2 font-bold focus-visible:ring-0"
+              autoFocus
+            />
+          </motion.div>
+        ) : (
+          <motion.h3
+            key="title"
+            {...fadeScaleAnimation}
+            style={{ transformOrigin: 'left' }}
+            className="line-clamp-1 grow text-sm font-bold"
+          >
+            {`${title} (${taskCount})`}
+          </motion.h3>
+        )}
+      </AnimatePresence>
 
       <div className="flex gap-2">
-        <IconButton onClick={toggleIsEditing} Icon={Icon} />
         <ConfirmDialog
           title="컬럼을 삭제할까요?"
           description="컬럼을 삭제하면 해당 컬럼에 있는 모든 작업이 삭제돼요."
           onConfirm={onDelete}
+          asChild
         >
-          <IconButton as="div" onClick={toggleIsEditing} Icon={Trash2} />
+          {!isEditing && <IconButton Icon={Trash2} />}
         </ConfirmDialog>
+        <IconButton onClick={toggleIsEditing} Icon={Icon} />
       </div>
     </div>
   );
