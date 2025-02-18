a07ef24a2f6e2b86b81b4d83c9aebe900ac2ff21
ColorFilter <johan@romantech.net>
2025-02-18 22:42:15 +0900

    Revert "컬럼 변경 테스트"
    This reverts commit c2756c7f810b268934da2d872a8960a9d963e006.

--- src/app/[boardId]/page.tsx
+++ src/app/[boardId]/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { Board } from '@/components';
-import { useHydration } from '@/hooks';
+import { useBoardRouteSync } from '@/hooks';
 
 export default function BoardPage() {
-  // useBoardRouteSync();
+  useBoardRouteSync();
 
-  const isHydrated = useHydration();
-  if (!isHydrated) return null;
+  // const isHydrated = useHydration();
+  // if (!isHydrated) return null;
 
   return <Board />;
 }
--- src/components/kanban-board/board-sidebar.tsx
+++ src/components/kanban-board/board-sidebar.tsx
@@ -6,12 +6,11 @@ import { SquareKanban } from 'lucide-react';
 import { BoardAddDialog } from '@/components';
 import { Button } from '@/components/ui/button';
 import { Sidebar } from '@/components/ui/sidebar';
-import { useBoardNavigation } from '@/hooks';
 
 const BoardSidebar = () => {
   const boards = useKanbanStore.use.boards();
+  const setCurrentBoard = useKanbanStore.use.setCurrentBoard();
   const currentBoardId = useKanbanStore.use.currentBoardId();
-  const changeBoard = useBoardNavigation();
 
   return (
     <Sidebar>
@@ -23,7 +22,7 @@ const BoardSidebar = () => {
               'cursor-pointer font-semibold text-baltic-400 transition-all hover:text-charade-100 duration-300 truncate py-2',
               { 'text-charade-100 font-bold': board.id === currentBoardId },
             )}
-            onClick={() => changeBoard(board.id)}
+            onClick={() => setCurrentBoard(board.id)}
             role="button"
           >
             <SquareKanban className="inline size-[26px] pr-2" />
@@ -32,7 +31,7 @@ const BoardSidebar = () => {
         ))}
       </ul>
       <div className="sticky bottom-0">
-        <BoardAddDialog onConfirm={(board) => changeBoard(board.id)}>
+        <BoardAddDialog>
           <Button className="w-full font-bold capitalize">add board</Button>
         </BoardAddDialog>
       </div>
