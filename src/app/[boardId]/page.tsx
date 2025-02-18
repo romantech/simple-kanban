acba7d4f08f498f5c5693c746651eda23e5ada98
ColorFilter <johan@romantech.net>
2025-02-18 22:44:11 +0900

    :art: layout.tsx에서 칸반 관련 컴포넌트 제외

--- src/app/[boardId]/page.tsx
+++ src/app/[boardId]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Board } from '@/components';
+import { Board, BoardSidebar, HeaderNav } from '@/components';
 import { useBoardRouteSync, useHydration } from '@/hooks';
 
 export default function BoardPage() {
@@ -9,5 +9,14 @@ export default function BoardPage() {
   const isHydrated = useHydration();
   if (!isHydrated) return null;
 
-  return <Board />;
+  return (
+    <div className="flex size-full flex-col">
+      <HeaderNav />
+      {/* min-h-0 으로 flex 아이템 최소 높이 제약 해제하고, flex-1 로 실제 사용 가능한 공간 계산 */}
+      <div className="flex min-h-0 flex-1">
+        <BoardSidebar />
+        <Board />
+      </div>
+    </div>
+  );
 }
