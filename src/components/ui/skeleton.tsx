9843a0dbbd7136c003520cf4d5d5091f5dd9363b
ColorFilter <johan@romantech.net>
2025-02-19 09:02:23 +0900

    :triangular_flag_on_post: 스켈레톤 로딩 스피너 추가

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@/components/ui/header';
 import { Sidebar } from '@/components/ui/sidebar';
+import { LoaderCircle } from 'lucide-react';
 
 export const Skeleton = () => {
   return (
@@ -7,6 +8,9 @@ export const Skeleton = () => {
       <Header />
       <div className="flex min-h-0 flex-1">
         <Sidebar />
+        <div className="grid size-full place-content-center">
+          <LoaderCircle width={100} height={100} className="animate-spin" />
+        </div>
       </div>
     </div>
   );
