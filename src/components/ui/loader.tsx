92e57693c726c9d9ffb9fafb23b584f7da9f8320
ColorFilter <johan@romantech.net>
2025-02-23 18:08:02 +0900

    :lipstick: Loader 사이즈 조정

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/lib';
 
 export const Loader = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div className={cn('*:progress *:size-8 *:bg-baltic-400 flex gap-4', className)} {...props}>
+    <div className={cn('*:progress *:size-6 *:bg-baltic-400 flex gap-4', className)} {...props}>
       <div />
       <div />
       <div />
