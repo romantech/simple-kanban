413fb1d512e6354215a29278b26dfd17c89a7bf1
ColorFilter <johan@romantech.net>
2025-02-24 17:33:24 +0900

    :triangular_flag_on_post: 사이드바 접기/펼치기 기능 추가

--- src/components/kanban-board/board-sidebar.tsx
+++ src/components/kanban-board/board-sidebar.tsx
@@ -2,42 +2,61 @@
 
 import { useKanbanStore } from '@/store';
 import { cn } from '@/lib';
-import { SquareKanban } from 'lucide-react';
+import { ChevronsLeft, SquareKanban } from 'lucide-react';
 import { BoardAddDialog } from '@/components';
 import { Button } from '@/components/ui/button';
 import { Sidebar } from '@/components/ui/sidebar';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import { Fragment, useState } from 'react';
 
 const BoardSidebar = () => {
   const boards = useKanbanStore.use.boards();
   const currentBoardId = useKanbanStore.use.currentBoardId();
+
+  const [openPanel, setOpenPanel] = useState(true);
   const router = useRouter();
 
+  const toggleSidebar = () => setOpenPanel((prev) => !prev);
+
   return (
-    <Sidebar>
-      <ul className="flex flex-col divide-y divide-baltic-900">
-        {Object.values(boards).map(({ title, id }) => (
-          <li
-            key={id}
-            className={cn(
-              'font-semibold text-baltic-400 transition-all hover:text-charade-100 duration-300 truncate py-2',
-              { 'text-charade-100 font-bold': id === currentBoardId },
-            )}
-          >
-            <Link href={{ pathname: id, query: { title } }} className="block" prefetch>
-              <SquareKanban className="inline size-[26px] pr-2" />
-              {title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className="sticky bottom-0">
-        <BoardAddDialog onConfirm={({ id, title }) => router.push(`${id}?title=${title}`)}>
-          <Button className="w-full font-bold capitalize">add board</Button>
-        </BoardAddDialog>
-      </div>
-    </Sidebar>
+    <Fragment>
+      <Sidebar
+        className={cn('hidden lg:block', { 'w-0 invisible opacity-0 border-none p-0': !openPanel })}
+      >
+        <ul className="flex flex-col divide-y divide-baltic-900">
+          {Object.values(boards).map(({ title, id }) => (
+            <li
+              key={id}
+              className={cn(
+                'font-semibold text-baltic-400 transition-all hover:text-charade-100 duration-300 truncate py-2',
+                { 'text-charade-100 font-bold': id === currentBoardId },
+              )}
+            >
+              <Link href={{ pathname: id, query: { title } }} className="block" prefetch>
+                <SquareKanban className="inline size-[26px] pr-2" />
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="sticky bottom-0">
+          <BoardAddDialog onConfirm={({ id, title }) => router.push(`${id}?title=${title}`)}>
+            <Button className="w-full font-bold capitalize">add board</Button>
+          </BoardAddDialog>
+        </div>
+      </Sidebar>
+      <button
+        type="button"
+        onClick={toggleSidebar}
+        className={cn(
+          'hidden h-[36px] px-3 bg-charade-950 lg:block transition-all duration-300 [&_svg]:size-6 absolute bottom-6 hover:bg-baltic-900',
+          { 'translate-x-[288px] rounded-r-xl': openPanel, 'rotate-180 rounded-l-xl': !openPanel },
+        )}
+      >
+        <ChevronsLeft />
+      </button>
+    </Fragment>
   );
 };
 
